a9985befdaba75be1c65837d45c347243acd507d
SergioChavarri <sergiochavarri7@gmail.com>
2023-11-16 14:23:38 -0300

    commit app se coloca boton

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  Button,
+  Alert,
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Esta sera mi App</Text>
+      <Button
+        title="PRESIONAME"
+        onPress={() => Alert.alert('MI PRIMER BOTON')}
+      />
       <StatusBar style="auto" />
     </View>
   );
