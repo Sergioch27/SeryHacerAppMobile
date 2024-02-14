745dbe31198b32a7a54f15259773175a54670b91
SergioChavarri <sergiochavarri7@gmail.com>
2024-02-14 14:58:32 -0300

    mejoras menores en la vista de podcast

--- src/screens/PodcastView.js
+++ src/screens/PodcastView.js
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native'
+import { StyleSheet, Image, SafeAreaView, Text } from 'react-native'
 import React from 'react'
+import Header from '../components/smart_components/Header'
 
 const PodcastView = () => {
   return (
     <SafeAreaView>
-      <Text>PodcastView</Text>
+       <Header />
+       <Image
+        style={styles.imageLogo}
+        source={{ uri: 'https://www.espacioseryhacer.com/wp-content/uploads/2024/02/1Mesa-de-trabajo-2-100.jpg' }}
+    />
+  <Text>PSILOCOGÍA PODCAST</Text>
     </SafeAreaView>
   )
 }
